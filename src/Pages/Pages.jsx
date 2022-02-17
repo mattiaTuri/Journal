2643ec964cd3184b4d93ac4cb76b05976eb3c6f3
mattiaTuri <85845784+mattiaTuri@users.noreply.github.com>
2022-02-17 17:47:22 +0100

    Update Pages.jsx

--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 function Pages() {
+
+  const currentLocation = useLocation()
+  console.log(currentLocation.pathname);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nytimes.com/svc/news/v3/content/all/arts.json?api-key=r2AHYUrfA3Tx5WPFUGltSMqASSPhXY4T"
+      )
+      .then((res) => {
+        console.log(res.data.results)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, );
+
   return (
     <section>
-      <h1>Page two</h1>
+      <h1>{currentLocation.pathname.replace("/", "")}</h1>
     </section>
   );
 }
