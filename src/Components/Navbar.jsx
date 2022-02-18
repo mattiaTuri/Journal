c5f59f0f0bb5115ce30ee1be05fa07ef864adaeb
Mattia <85845784+mattiaTuri@users.noreply.github.com>
2022-02-18 17:24:08 +0100

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,53 +1,25 @@
-import { Link, NavLink } from "react-router-dom";
-import useFetch from "../Hooks/useFetch";
+import { NavLink } from "react-router-dom";
 
-function Navbar({ isHamburgerOpen }) {
-  const { sections } = useFetch(
-    "https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=r2AHYUrfA3Tx5WPFUGltSMqASSPhXY4T"
-  );
+function Navbar() {
 
   return (
-    <>
-      <nav className="navbar-desktop">
+    <nav className="navbar">
         <NavLink className="nav-link" to="/arts">
           Arts
         </NavLink>
         <NavLink className="nav-link" to="/business">
           Business
         </NavLink>
+        <NavLink className="nav-link" to="/science">
+          Science
+        </NavLink>
         <NavLink className="nav-link" to="/sports">
           Sports
-        </NavLink>
-        <NavLink className="nav-link" to="/tech">
-          Tech
         </NavLink>
         <NavLink className="nav-link" to="/world">
           World
         </NavLink>
-      </nav>
-      <div
-        className={
-          isHamburgerOpen
-            ? "navigation-menu-left active"
-            : "navigation-menu-left"
-        }
-      >
-        <h3>Sections</h3>
-        <div className="nav-menu-link">
-          {sections.slice(1).map((section) => {
-            return (
-              <Link
-                key={section.section}
-                className="nav-link"
-                to={`/${section.section}`}
-              >
-                {section.display_name}
-              </Link>
-            );
-          })}
-        </div>
-      </div>
-    </>
+      </nav> 
   );
 }
 
