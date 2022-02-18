c34ef3908f126ad5bc0189e64f67e668d628d011
Mattia <85845784+mattiaTuri@users.noreply.github.com>
2022-02-18 17:23:14 +0100

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,28 +1,49 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Navbar from "./Navbar";
-import Burger from "./Burger";
-import header from "../Assets/css/header.css";
+import useFetch from "../Hooks/useFetch";
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+import { Drawer } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from '@mui/material/Typography';
+import header from "../Assets/css/header.css"
 
 function Header() {
-  const [isHamburgerOpen, setHamburger] = useState(false);
+  const [icon, setIcon] = useState(false);
+  const [menu, setMenu] = useState(false)
+  const {sections} = useFetch("https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=r2AHYUrfA3Tx5WPFUGltSMqASSPhXY4T");
 
-  const showSidebar = () => {
-    setHamburger(!isHamburgerOpen);
-  };
+  const toggleLeftMenu = (open) => (event) => {
+    setIcon(!icon)
+    setMenu(open)
+  }
 
   return (
-    <header>
-      <section className="header-container">
-        <div className="header-title">
-          <Link className="nav-link" to="/">
-            World Journal
-          </Link>
-        </div>
-        <Burger isHamburgerOpen={isHamburgerOpen} showSidebar={showSidebar} />
-      </section>
-      <Navbar isHamburgerOpen={isHamburgerOpen} />
-    </header>
+      <AppBar className="header" color="" position="static" sx={{ boxShadow: 0 }}>
+        <Toolbar className="header-container" variant="dense" sx={{justifyContent:"center"}}>
+          <Typography className="header-title" variant="h1">World Journal</Typography>
+          <IconButton className="header-icon" onClick={toggleLeftMenu(true)}>
+            {icon ? <CloseIcon/> : <MenuIcon />}
+          </IconButton>
+        </Toolbar>
+        <Navbar/>
+        <Drawer anchor={"left"} open={menu} onClose={toggleLeftMenu(false)} sx={{width:300}}>
+          {sections.map(section => {
+            return (
+              <Link
+                key={section.section}
+                className="nav-link"
+                to={`/${section.section}`}
+              >
+                {section.display_name}
+              </Link>
+            );
+          })}
+        </Drawer>
+      </AppBar>
   );
 }
 
