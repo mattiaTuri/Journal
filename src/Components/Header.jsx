3ea5ebb6bafed5069adec620dffd6cc7c71aec0b
Mattia <85845784+mattiaTuri@users.noreply.github.com>
2022-02-21 16:57:49 +0100

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,12 +8,18 @@ import { Drawer } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import Typography from "@mui/material/Typography";
+import { useLocation } from "react-router-dom";
+import { Container } from "@mui/material";
 import header from "../Assets/css/header.css";
 import moment from "moment";
 
 function Header() {
   let date = moment().format("LL");
+  const currentLocation = useLocation();
+  const sectionPath = currentLocation.pathname.replace("/", "");
 
   const [icon, setIcon] = useState(false);
   const [menu, setMenu] = useState(false);
@@ -27,20 +33,26 @@ function Header() {
   };
 
   return (
+    <Container>
     <AppBar className="header" color="" position="static" sx={{ boxShadow: 0 }}>
       <Toolbar
         className="header-container"
         variant="dense"
         sx={{ justifyContent: "center" }}
       >
+        <div className="date date-desktop">
         <Typography
-          className="date date-desktop"
+          className="date"        
           variant="button"
           display="block"
           gutterBottom
         >
           {date}
         </Typography>
+        <Typography variant="body2" gutterBottom>
+          Article section: <strong>{sectionPath}</strong>
+        </Typography>
+        </div>
         <Typography className="header-title" variant="h1">
           World Journal
         </Typography>
@@ -55,19 +67,25 @@ function Header() {
         onClose={toggleLeftMenu(false)}
         sx={{ width: 300 }}
       >
+        <Typography variant="h6" component="h6" sx={{paddingLeft: 2}}>Section List</Typography>
+        <List>          
         {sections.slice(1).map((section) => {
           return (
-            <Link
-              key={section.section}
-              className="link"
-              to={`/${section.section}`}
-            >
-              {section.display_name}
-            </Link>
+            <ListItem>
+              <Link
+                key={section.section}
+                className="link"
+                to={`/${section.section}`}
+              >
+                {section.display_name}
+              </Link>
+            </ListItem>
           );
         })}
+        </List>       
       </Drawer>
     </AppBar>
+    </Container>
   );
 }
 
