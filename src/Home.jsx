28ddfa41b4df078e6d2d4b378e5bca3a4ea1a265
Mattia <85845784+mattiaTuri@users.noreply.github.com>
2022-02-23 17:47:44 +0100

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, {createContext } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import { Container } from "@mui/material";
 
+export const SectionContext = createContext()
+
 function Home() {
+
+  const currentLocation = useLocation();
+  let sectionPath;
+
+  if (currentLocation.pathname === "/") {
+    sectionPath = "home";
+  } else {
+    sectionPath = currentLocation.pathname.replace("/", "");
+  }
+
   return (
-    <Container>
-      <Header />
-      <Outlet />
-      <Footer />
-    </Container>
+    <SectionContext.Provider value={sectionPath}>
+      <Container>       
+        <Header/>
+        <Outlet />
+        <Footer />   
+      </Container>
+    </SectionContext.Provider>
   );
 }
 
