a0ba6adc5148c54ab8b292992c6c4c19fe89ce2a
Mattia <85845784+mattiaTuri@users.noreply.github.com>
2022-02-24 15:23:00 +0100

    Update Articles.jsx

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -7,74 +7,87 @@ import { Card, CardContent, CardMedia } from "@mui/material";
 import { Link } from "@mui/material";
 import header from "../Assets/css/articles.css";
 import { SectionContext } from "../Home";
+import Loading from "./Loading";
+import Error from "./Error";
 
 function Articles() {
   const sectionPath = useContext(SectionContext);
 
-  const { articles } = useAxios(
+  const { articles, loading, status } = useAxios(
     "https://api.nytimes.com/svc/topstories/v2/" +
       sectionPath +
       ".json?api-key=r2AHYUrfA3Tx5WPFUGltSMqASSPhXY4T"
   );
 
-  return (
-    <div className="section-article">
-      {articles.map((article, i) => {
-        if (i === 0 && article.multimedia != null) {
-          return (
-            <Card
-              key={i}
-              sx={{ marginTop: 20, position: "relative" }}
-              className="main-article"
-            >
-              <CardContent
-                className="main-article-content"
-                sx={{ position: "absolute" }}
-              >
-                <Typography
-                  variant="h5"
-                  element="h1"
-                  sx={{ fontWeight: "bold" }}
-                >
-                  {article.title || "Latest news"}
-                </Typography>
-                <Typography
-                  variant="subtitle1"
-                  component="body1"
-                  sx={{ fontWeight: "bold" }}
-                  gutterBottom
-                >
-                  {article.abstract}
-                </Typography>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  <Link href="#">Continue reading</Link>
-                </Typography>
-              </CardContent>
-              <CardMedia
-                className="main-article-image"
-                component="img"
-                image={article.multimedia[0].url}
-              />
-            </Card>
-          );
-        }
-      })}
-      <div className="sub-article">
+  if(loading){
+    return(
+      <Loading/>
+    )
+  }else if(status == 404){
+    return(
+      <Error/>
+    )
+  }else{
+    return (
+      <div className="section-article">
         {articles.map((article, i) => {
-          if (i !== 0 && article.title != "" && article.multimedia != null) {
+          if (i === 0 && article.multimedia != null) {
             return (
-              <Article
+              <Card
                 key={i}
-                title={article.title}
-                url={article.multimedia[0].url}
-                description={article.abstract}
-              />
+                sx={{ marginTop: 20, position: "relative" }}
+                className="main-article"
+              >
+                <CardContent
+                  className="main-article-content"
+                  sx={{ position: "absolute" }}
+                >
+                  <Typography
+                    variant="h5"
+                    element="h1"
+                    sx={{ fontWeight: "bold" }}
+                  >
+                    {article.title || "Latest news"}
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    component="body1"
+                    sx={{ fontWeight: "bold" }}
+                    gutterBottom
+                  >
+                    {article.abstract}
+                  </Typography>
+                  <Typography sx={{ fontWeight: "bold" }}>
+                    <Link href={article.url} target="_blank">Continue reading</Link>
+                  </Typography>
+                </CardContent>
+                <CardMedia
+                  className="main-article-image"
+                  component="img"
+                  image={article.multimedia[0].url}
+                />
+              </Card>
             );
           }
         })}
+        <div className="sub-article">
+          {articles.map((article, i) => {
+            if (i !== 0 && article.title != "" && article.multimedia != null) {
+              return (
+                <Article
+                  key={i}
+                  title={article.title}
+                  img={article.multimedia[0].url}
+                  description={article.abstract}
+                  info={article.url}
+                />
+              );
+            }
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Articles;
