7f444452dbc4b1c234a391d2cb17c1711ae356d0
Mattia <85845784+mattiaTuri@users.noreply.github.com>
2022-02-24 15:22:41 +0100

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import Box from '@mui/material/Box';
 
 function Error() {
-  return <h1>No article found</h1>;
+  return (
+    <Box className="error">
+      <Typography variant="h4" component="h1">
+      No article found
+      </Typography>
+    </Box>
+  )
 }
 
 export default Error;
