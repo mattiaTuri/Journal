eb66cc9970bb2a7cd40c85dd44aa7e0279dd7cb6
Mattia <85845784+mattiaTuri@users.noreply.github.com>
2022-02-24 15:27:12 +0100

    Update useAxios.js

--- src/Hooks/useAxios.js
+++ src/Hooks/useAxios.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Error from "../Pages/Error";
 
 function useAxios(url) {
   const [sections, setSections] = useState([]);
